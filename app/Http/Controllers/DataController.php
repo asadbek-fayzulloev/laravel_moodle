36942ff08a535b2777569c4e0915f5c3b83f669b
Asadbekinha <fayzulloevasadbek@gmail.com>
2021-10-03 21:26:50 +0500

    dynamic use of pieChart

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    public function pieChartData(){
+        return [
+            "hoverBackgroundColor" => "red",
+            "hoverBorderWidth" => 10,
+            "labels" => ["Green", "Red", "Blue"],
+            "datasets" => array([
+                "label" => "Data One",
+                "backgroundColor" => ["#41B883", "#E46651", "#00D8FF"],
+                "data" => [5, 5, 10]
+            ])
+        ];
+
+    }
+}
---
+++ resources/js/components/ChartpieComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <pie-chart :chart-data="data" :options="{responsive: true, maintainAspectRatio: true}"></pie-chart>
+  </div>
+</template>
+
+<script>
+import PieChart from "./PieChart.js";
+export default {
+  components: {
+    PieChart
+  },
+  data: function() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+      this.update()
+  },
+  methods: {
+    update: function () {
+        axios.get('/data/chart').then((response) => {
+            this.data = response.data
+        });
+    }
+  }
+
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
