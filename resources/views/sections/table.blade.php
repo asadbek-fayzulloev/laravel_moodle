6c46f6512d4de49b4463388fb7c0dc99e43ddf0c
Asadbekinha <fayzulloevasadbek@gmail.com>
2021-10-15 12:26:19 +0500

    table style change

--- resources/views/sections/table.blade.php
+++ resources/views/sections/table.blade.php
@@ -139,9 +139,7 @@
     </div>
 
 </div>
-{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script> --}}
 <script>
     $(document).ready(function() {
         $('#example').DataTable( {
